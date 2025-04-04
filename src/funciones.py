58228d4623cfa655fc56c3135e9bf6664042afa9
Juanfran1-1 <juanucedac@gmail.com>
2025-04-04 00:12:40 -0300

    agrego tercera funcion('mostrar_ronda')  y modifico la funcion 'procesar_rondas' para mostrar la ronda actual

--- src/funciones.py
+++ src/funciones.py
@@ -4,10 +4,20 @@ def calcular_puntos(kills,assists,deaths):
     puntaje = {'kill' : 3 , 'assist': 1 , 'death': -1}
     return (kills * puntaje['kill']) + (assists * puntaje['assist']) + (puntaje['death'] if deaths else 0)
 
+def mostrar_ronda(stats_ronda,mvps,i,rm):
+    ordenados= sorted(stats_ronda.items(), key= lambda x: x[1]['points'], reverse=True)
+    print(f"\nRanking ronda {i}") if i < rm else print('\nRanking final')
+    print(f"{'jugador':<10} {'kills':<5} {'asistencias':<5} {'muertes':<5} {' mvps':<5} {'puntos':<6}")
+    print('-'*60)
+    for jugador,stats in ordenados:
+        print(f"{jugador:<10} {stats['kills']:>5} {stats['assists']:>11} {stats['deaths']:>7} {mvps[jugador]:>5} {stats['points']:>6}")
+    print("-" * 60)
+
 def procesar_rondas(rounds):
     mvps = {}
     stats_ronda= {}
     i= 0
+    rm = len(rounds)
     for ronda in rounds:
         i+=1
         puntajes_ronda = {}
@@ -29,3 +39,4 @@ def procesar_rondas(rounds):
                 max = puntajes_ronda[jugador]
                 mvp = jugador 
         mvps[mvp]=+ 1
+        mostrar_ronda(stats_ronda,mvps,i,rm)
