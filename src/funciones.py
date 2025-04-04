2993010b0f6f688c815609b0d52bbb10d13b73b4
Juanfran1-1 <juanucedac@gmail.com>
2025-04-04 00:03:53 -0300

    agrego primera funcion para el calculo de puntos en base a estadisticas

--- src/funciones.py
+++ src/funciones.py
@@ -1,1 +1,5 @@
 #FUNCIONES PARA MAIN
+
+def calcular_puntos(kills,assists,deaths):
+    puntaje = {'kill' : 3 , 'assist': 1 , 'death': -1}
+    return (kills * puntaje['kill']) + (assists * puntaje['assist']) + (puntaje['death'] if deaths else 0)
